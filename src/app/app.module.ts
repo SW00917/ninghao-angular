149c91ce9b6bb515fdf16ab32e9f53f6d6bb903b
weihsu <weihsu@capgemini.com>
2023-09-04 15:00:18 +0800

    User module with register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostModule } from './modules/post/post.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PostModule],
+  imports: [BrowserModule, AppRoutingModule, PostModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
