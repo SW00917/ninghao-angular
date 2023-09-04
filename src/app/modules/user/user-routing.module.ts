fe92f6e175afe3c9822b6639562066a41e3543c1
weihsu <weihsu@capgemini.com>
2023-09-04 17:21:52 +0800

    Feature module routing

---
+++ src/app/modules/user/user-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  // declarations: [],
+  // imports: [CommonModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class UserRoutingModule {}
