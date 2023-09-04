742da4ff02e71b365eb902bc812ad69677a9e276
weihsu <weihsu@capgemini.com>
2023-09-04 12:05:41 +0800

    paramMap

--- src/app/modules/post/components/post-details/post-details.component.ts
+++ src/app/modules/post/components/post-details/post-details.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
-  styleUrls: ['./post-details.component.css']
+  styleUrls: ['./post-details.component.css'],
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const postId = Number(params.get('id'));
+      console.log(postId);
+    });
+  }
 }
