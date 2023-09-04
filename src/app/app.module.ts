9b36860b70315c0fbc272fbe75ca7dc9993d53bc
weihsu <weihsu@capgemini.com>
2023-09-04 17:42:13 +0800

    DemoModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
 import { HeaderComponent } from './core/components/header/header.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { DemoModule } from './modules/demo/demo.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PageNotFoundComponent],
   // imports: [BrowserModule, AppRoutingModule, PostModule, UserModule],
-  imports: [BrowserModule, PostModule, UserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    PostModule,
+    UserModule,
+    DemoModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
