8e7aee81566a0a1eb6ca628495849e7230f053aa
weihsu <weihsu@capgemini.com>
2023-09-04 15:30:26 +0800

    Reactive Forms

--- src/app/modules/user/components/register/register.component.ts
+++ src/app/modules/user/components/register/register.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  username = new FormControl();
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule],
 })
-export class UserModule { }
+export class UserModule {}
