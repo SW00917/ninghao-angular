dcd3e92d740d59035e61f5d90709801f17faec54
weihsu <weihsu@capgemini.com>
2023-09-05 10:59:19 +0800

    Admin component

--- src/app/modules/demo/demo.module.ts
+++ src/app/modules/demo/demo.module.ts
@@ -5,13 +5,15 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { DemoComponent } from './demo.component';
 import { ChildRoutesComponent } from './child-routes/child-routes.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
   declarations: [
     DemoComponent,
     ChildRoutesComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule,
