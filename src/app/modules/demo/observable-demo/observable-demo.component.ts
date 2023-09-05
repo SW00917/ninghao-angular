208c44040163e9e3f1a9833315ab2bc64bcc7865
weihsu <weihsu@capgemini.com>
2023-09-05 15:20:46 +0800

    Observable

--- src/app/modules/demo/observable-demo/observable-demo.component.ts
+++ src/app/modules/demo/observable-demo/observable-demo.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo',
   templateUrl: './observable-demo.component.html',
-  styleUrls: ['./observable-demo.component.css']
+  styleUrls: ['./observable-demo.component.css'],
 })
-export class ObservableDemoComponent {
+export class ObservableDemoComponent implements OnInit {
+  demoObservable!: Observable<String>;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.demoObservable = of('小火龍', '皮卡丘', '超夢');
+  }
+
+  onClick() {
+    this.demoObservable.subscribe((value) => console.log(value));
+  }
 }
