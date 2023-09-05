08b5e0870b900c70b76031ff6f47e3dbc8fafa89
weihsu <weihsu@capgemini.com>
2023-09-05 12:10:25 +0800

    Resolve

--- src/app/modules/post/post-routing.module.ts
+++ src/app/modules/post/post-routing.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { PostDetailResolveService } from './services/post-detail-resolve.service';
 
 const routes: Routes = [
   { path: 'posts', component: PostComponent },
-  { path: 'posts/:id', component: PostDetailsComponent },
+  {
+    path: 'posts/:id',
+    resolve: {
+      entity: PostDetailResolveService,
+    },
+    component: PostDetailsComponent,
+  },
 ];
 
 @NgModule({
