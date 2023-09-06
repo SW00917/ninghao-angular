097a134359744e4aa01fa1ce755bb12101f391c6
weihsu <weihsu@capgemini.com>
2023-09-06 11:12:37 +0800

    PostEditComponent

--- src/app/modules/post/post.module.ts
+++ src/app/modules/post/post.module.ts
@@ -5,9 +5,15 @@ import { PostItemComponent } from './components/post-item/post-item.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
 // import { AppRoutingModule } from 'src/app/app-routing.module';
 import { PostRoutingModule } from './post-routing.module';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
 @NgModule({
-  declarations: [PostComponent, PostItemComponent, PostDetailsComponent],
+  declarations: [
+    PostComponent,
+    PostItemComponent,
+    PostDetailsComponent,
+    PostEditComponent,
+  ],
   // imports: [CommonModule, AppRoutingModule, PostRoutingModule],
   imports: [CommonModule, PostRoutingModule],
   exports: [PostComponent],
